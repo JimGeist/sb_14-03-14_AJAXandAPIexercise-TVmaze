e4d7183e46305d0ce2d45ddd4a59b92677b00828
Jim Geist <info@jimgeist.com>
2021-01-09 20:23:46 -0500

    v0.01: Working version of TV maze API front end application. Next step is to see if the api calls can become a common function that either searches for shows OR gets the episodes.

--- tvmaze.js
+++ tvmaze.js
@@ -2,35 +2,106 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
- *       will be returning a promise.
+ *       will return a promise.
  *
  *   - Returns an array of objects. Each object should include
  *     following show information:
  *    {
-        id: <show id>,
-        name: <show name>,
-        summary: <show summary>,
-        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
+        id: <text, show id>,
+        name: <text, show name>,
+        summary: <text, show summary>,
+        image: <object, medium and original image from the show data. null / no image is possible>
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  // Make an ajax request to the tv maze searchShows api. 
+  // query must have a value 
+
+  // replace embedded spaces with % 20
+  let fixedSearch = query.split(" ").join("%20");
+
+  try {
+    const url = `http://api.tvmaze.com/search/shows?q=${fixedSearch}`;
+    const res = await axios.get(url);
+
+    if (res.status = 200) {
+      if (res.data.length > 0) {
+        const outShows = [];
+        for (let showObj of res.data) {
+          outShows.push(buildShowObject(showObj.show.id, showObj.show.name, showObj.show.summary, showObj.show.image))
+        }
+        if (outShows.length === 0) {
+          outShows.push(buildShowObject(0, "We are experiencing operating difficulties...",
+            `Something bad happened while processing the ${res.data.length} show(s) found for search of '${query}'.`,
+            { medium: "./images/VectorStock.com-18175384.jpg" }));
+        }
+        return outShows;
+
+      } else {
+        // nothing was found
+        return [buildShowObject(0, "Oh SNAP!",
+          `No shows were found for '${query}'. Please change your search and try again. <br><br>TVmaze response code = ${res.status}.`, "")];
+      }
+
+    } else {
+      return [buildShowObject(0, "We are experiencing operating difficulties...",
+        `Show search for '${query}' was not successful. <br><br>(TVmaze response code = ${res.status}).`,
+        { medium: "./images/VectorStock.com-18175384.jpg" })];
     }
-  ]
+
+  } catch (e) {
+    return [buildShowObject(0, "We are experiencing operating difficulties...",
+      `An unexpected error (${e.message}) occurred while connecting to TVmaze. Search for '${query}' was not performed.`,
+      { medium: "./images/VectorStock.com-18175384.jpg" })];
+  }
+
 }
 
+
+/** Build Show Object
+ *
+ *   - Returns a show object that includes the following 
+ *      show information:
+ *    {
+        id: <show id>,
+        name: <show name>,
+        summary: <show summary>,
+        image: <an image from the show data, or a default image if no image exists, (image isn't needed until later)>
+      }
+ */
+function buildShowObject(id, name, summary, inImageObject) {
+
+  // do something with the image to ensure we have one to return.
+  // Images: http://www.tvmaze.com/api#show-image
+  //  look for medium, then original and finally use the tv image when there is 
+  //  no image.
+  const imgUrlMed = {};
+
+  if (inImageObject) {
+    if (inImageObject.medium) {
+      // we have an inImageObject.
+      imgUrlMed.medium = inImageObject.medium;
+      //inImageObject.original);
+
+    } else {
+      imgUrlMed.medium = "./images/tv-missing.png";
+    }
+  } else {
+    imgUrlMed.medium = "./images/tv-missing.png";
+  }
+
+  return {
+    id,
+    name,
+    summary,
+    image: imgUrlMed.medium
+  };
+
+}
 
 
 /** Populate shows list:
@@ -43,11 +114,13 @@ function populateShows(shows) {
 
   for (let show of shows) {
     let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+      `<div class=" col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+             <img class="card-img-top" src="${show.image}">
+             <a href="#" class="d-block mt-3 btn btn-lg btn-primary">Episodes</a>
            </div>
          </div>
        </div>
@@ -58,15 +131,131 @@ function populateShows(shows) {
 }
 
 
+/** get Episodes
+ *    - given a Show Id, get the list of episoded for the tv show. The function is 
+ *       async show it will return a promise.
+ *
+ *   - Returns an array of objects. Each object should include
+ *     following show information:
+ *    {
+        id: <number, id number for episode>
+        name: <text, episode name>,
+        season: <number, season number>,
+        number: <number, episode number for the season>,
+        summary: <text, episode summary>,
+        url: <url for punchout to the tv maze page about the episode>
+      }
+ */
+async function getEpisodes(showId) {
+
+  // Make an ajax request to the tv maze shows api for an episode list. 
+  // showId must have a value and this should be checked in the function that calls
+  //  searchEpisodes
+
+
+  try {
+    const url = `http://api.tvmaze.com/shows/${showId}/episodes`;
+    const res = await axios.get(url);
+
+    console.dir(res);
+
+    if (res.status = 200) {
+      if (res.data.length > 0) {
+        const outEpisodes = buildEpisodesArray(res.data);
+        // for (let episodeObj of res.data) {
+        //   outEpisodes.push(buildShowObject("", "", "", "", ""));
+        // }
+        // if (outEpisodes.length === 0) {
+        //   // outEpisodes.push(buildShowObject(0, "We are experiencing operating difficulties...",
+        //   //   `Something bad happened while processing the ${res.data.length} episodes for show(s) found for search of '${showId}'.`,
+        //   //   { medium: "./images/VectorStock.com-18175384.jpg" }));
+        // }
+        return outEpisodes;
+
+      } else {
+        // nothing was found
+        // return [buildShowObject(0, "Oh SNAP!",
+        //   `No shows were found for '${showId}'. Please change your search and try again. <br><br>TVmaze response code = ${res.status}.`, "")];
+      }
+
+    } else {
+      // return [buildShowObject(0, "We are experiencing operating difficulties...",
+      //   `Show search for '${showId}' was not successful. <br><br>(TVmaze response code = ${res.status}).`,
+      //   { medium: "./images/VectorStock.com-18175384.jpg" })];
+    }
+
+  } catch (e) {
+    // return [buildShowObject(0, "We are experiencing operating difficulties...",
+    //   `An unexpected error (${e.message}) occurred while connecting to TVmaze. Search for '${showId}' was not performed.`,
+    //   { medium: "./images/VectorStock.com-18175384.jpg" })];
+  }
+
+}
+
+
+/** Build Episodes Object
+ *    - builds an episodes array of object that includes the following information:
+ *    {
+        name: <text, episode name>,
+        season: <number, season number>,
+        number: <number, episode number for the season>,
+        summary: <text, episode summary>,
+        url: <url for punchout to the tv maze page about the episode>
+      }
+ */
+function buildEpisodesArray(inEpisodes) {
+
+  const outEpisodes = [];
+
+  for (let episode of inEpisodes) {
+    outEpisodes.push({
+      id: episode.id,
+      name: episode.name,
+      season: episode.season,
+      number: episode.number,
+      summary: episode.summary,
+      url: episode.url
+    });
+  }
+
+  return outEpisodes;
+
+}
+
+
+/** Populate Episodes List
+ *  creates a list entry for each episode in the show.
+ *   
+ */
+function populateEpisodesList(inEpisodes, inShowId) {
+
+  const $episodesList = $("#episodes-list");
+  $episodesList.empty();
+
+  for (let episode of inEpisodes) {
+    let $listItem = $(
+      `<li class="" data-episode-id="${inShowId}-${episode.id}">
+        "${episode.name}" (season ${episode.season}, number ${episode.number}) 
+        <a href="${episode.url}" target="${inShowId}">more...</a></li>
+      `);
+
+    $episodesList.append($listItem);
+  }
+
+
+}
+
+
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
+$("#search-form").on("submit", async function handleSearch(evt) {
   evt.preventDefault();
 
-  let query = $("#search-query").val();
+  // trim any leading and trailing spaces from the value entered in search query.
+  let query = $("#search-query").val().trim();
   if (!query) return;
 
   $("#episodes-area").hide();
@@ -77,14 +266,26 @@ $("#search-form").on("submit", async function handleSearch (evt) {
 });
 
 
-/** Given a show ID, return list of episodes:
- *      { id, name, season, number }
- */
+/** Clicking on Episodes 'button' will perform episode search 
+ * 
+*/
+$("#shows-list").on("click", "a", async function () {
 
-async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+  const $topShowDiv = $(this).parent().parent().parent();
 
-  // TODO: return array-of-episode-info, as described in docstring above
-}
+  const showId = +$(this).parent().parent().attr("data-show-id");
+
+  if (showId > 0) {
+    let episodes = await getEpisodes(showId);
+    populateEpisodesList(episodes, showId);
+
+    // hide all shows except for the show we have episodes for
+    $("div.col-md-6.col-lg-3").css("display", "none");
+    $($topShowDiv).css("display", "");
+
+    // Unhide the episode portion of the page
+    $("#episodes-area").css("display", "");
+
+  }
+
+})
